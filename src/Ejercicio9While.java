334a08c6d589d723dacf7007d58a214ab5d73e5f
wilmerpe <wilmer.wmpac@gmail.com>
2022-11-10 19:33:59 +0100

    Initial commit

---
+++ src/Ejercicio9While.java
@@ -0,0 +1,12 @@
+public class Ejercicio9While {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Secuencia de números");
+        System.out.println("Del 100 al 0");
+        System.out.println("Serie: ");
+        for (num =100; num>=0;num-=7)
+        {
+            System.out.println(num+ ",");
+        }
+    }
+}
